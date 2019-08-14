5cd0661d299229fbf8166c24734d0e3ada1ed097
Alice Fabre <alice.fabre@hotmail.fr>
2019-08-14 11:44:26 +0200

    validations dans les models + belongs_to / has_many

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,12 @@
 class Restaurant < ApplicationRecord
+  CATEGORY = ["chinese", "italian", "japanese", "french", "belgian"]
+  # A review must have a parent restaurant.
+  # When a restaurant is destroyed, all of its reviews should be destroyed as well.
+  has_many :reviews, dependent: :destroy
+  # A restaurant must have at least a name, an address and a category.
+  # The restaurant category should belong to a fixed list ["chinese", "italian", "japanese", "french", "belgian"]
+  # When a restaurant is destroyed, all of its reviews should be destroyed as well.
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true, inclusion: { in: CATEGORY }# inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ApplicationRecord
+  RATING = (0..5).to_a
+  # A review must have a parent restaurant.
   belongs_to :restaurant
+  # A review must have content and a rating. The rating should be a number between 0 and 5.
+  validates :content, presence: true
+  validates :rating, presence: true, inclusion: { in: RATING }, numericality: { only_integer: true }
 end
